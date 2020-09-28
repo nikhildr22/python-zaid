7af592f03d0677ab878f5e59bed393788631890b
nikhildr22 <nikhildr22@gmail.com>
2020-09-28 19:22:24 -0400

    Changed interface to wlan0 Added python 3 string formatting

--- 1 mac_changer/macchanger-2.py
+++ 1 mac_changer/macchanger-2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''
 Storing the interface and mac addr to a variable and 
 then pass the value to the command directly
@@ -6,12 +6,12 @@ then pass the value to the command directly
 
 import subprocess
 
-interface = "enp2s0"
+interface = "wlan0"
 macaddr = "00:11:22:33:44:55"
 
-print "[+] Changing Mac Address of Interface %s to %s"%(interface,macaddr)
+print(f"[+] Changing Mac Address of Interface {interface} to {macaddr}")
 
 
-subprocess.call("ifconfig %s down"%interface,shell=True)
-subprocess.call("ifconfig %s hw ether %s"%(interface,macaddr),shell=True)
-subprocess.call("ifconfig %s up"%interface,shell=True)
+subprocess.call(f"ifconfig {interface} down",shell=True)
+subprocess.call(f"ifconfig {interface} hw ether {macaddr}",shell=True)
+subprocess.call(f"ifconfig {interface} up",shell=True)
