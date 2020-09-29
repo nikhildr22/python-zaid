1fb7578fdb736a26991a7a1c2ac29efdd39a8103
nikhildr22 <nikhildr22@gmail.com>
2020-09-28 22:32:26 -0400

    python3 support

--- 1 mac_changer/macchanger-5.py
+++ 1 mac_changer/macchanger-5.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''
 Getting the value for  the interface and mac addr to a variable and 
 then pass the value to the command directly
@@ -17,24 +17,24 @@ python macchanger.py --help to print help'''
 import subprocess
 import optparse
 
-parser=optparse.OptionParser()	#init the parser object
+parser=optparse.OptionParser()  # init the parser object
 parser.add_option("-i","--interface",dest="interface",help="Interface to change the mac address")
-#adding the options like -i or --interface switches, dest this where the passed values get saved and help display the help msg python macchanger.py --help
+# adding the options like -i or --interface switches, dest this where the passed values get saved and help display the help msg python macchanger.py --help
 
 parser.add_option("-m","--mac",dest="new_mac",help="add new mac address")
 
 (options,arguments)=parser.parse_args()	
-#the funtion returns a value to this 2 varible options and arguments
-#options is nobut the wlan0 and aa:bb:cc:dd:ee:ff
-#arguments is nothingbut --interface and --mac or -i and -m
+# the funtion returns a value to this 2 varible options and arguments
+# options is nothing but the wlan0 and aa:bb:cc:dd:ee:ff
+# arguments is nothing but --interface and --mac or -i and -m
 
 interface = options.interface
 macaddr = options.new_mac
 
-#options contains the value to get the value we call options.interface and options.new_mac
+# options contain the value to get the value we call options.interface and options.new_mac
 
 subprocess.call(["ifconfig",interface,"down"])
 subprocess.call(["ifconfig",interface,"hw","ether",macaddr])
 subprocess.call(["ifconfig",interface,"up"])
 
-print "[+] Changing Mac Address of Interface %s to %s"%(interface,macaddr)
+print(f"[+] Changing Mac Address of Interface {interface} to {macaddr}")
